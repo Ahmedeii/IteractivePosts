e5428abb1d4d285c4aceea35c6a7d438a32a6f7f
Ahmedeii <ahmedabdalnabe77@gmail.com>
2024-09-10 01:07:01 +0300

    a

--- src/index.js
+++ src/index.js
@@ -4,25 +4,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+
 } from "react-router-dom";
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-  
-//       {/* ... etc. */}
-//     </Route>
-//   )
-// );
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
     <App />
-    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>
 );
 
